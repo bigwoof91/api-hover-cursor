63d6d9e79a045a8a22f18e6ca73e90e983ca3a6c
Michael Wolf <wolf@Michaels-MBP.fios-router.home>
2019-02-10 01:47:18 -0500

    converted functions to singleton...

--- src/lib/cursor.js
+++ src/lib/cursor.js
@@ -1,37 +1,33 @@
-function getCursorXY(log = false) {
-	if (window.Event) { // for old browsers; https://developer.mozilla.org/en-US/docs/Web/API/Window/captureEvents
-	document.captureEvents(Event.MOUSEMOVE);
-	}
-	document.onmousemove = (e) => getCoordinates(e, log);
-}
 
-function logXY({ x, y }) {
-  return console.log(`positionX: ${x}, positionY: ${y}`);
-}
+export default class Cursor {
+  getCursorXY = (log = false) => {
+    if (window.Event) { // for old browsers; https://developer.mozilla.org/en-US/docs/Web/API/Window/captureEvents
+    document.captureEvents(Event.MOUSEMOVE);
+    }
+    document.onmousemove = (e) => getCoordinates(e, log);
+  };
+  
+  logXY = ({ x, y }) => console.log(`positionX: ${x}, positionY: ${y}`);
+  
+  getCoordinates = (e, doLog = false) => {
+    var y = getCursorY(e);
+    var x = getCursorX(e);
+    if (doLog) logXY({x, y})
+    return {
+      x,
+      y,
+    }
+  };
+  
+  getCursorY = (e) => (window.Event) 
+      ? e.pageY
+        : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
 
-function getCoordinates(e, log = false) {
-  var y = getCursorY(e);
-  var x = getCursorX(e);
-  if (log) logXY({x, y})
-  return {
-    x,
-    y,
-  }
-}
+  
+  getCursorX = (e) => (window.Event) 
+      ? e.pageX 
+        : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+  };
 
-function getCursorY(e) {
-  console.log(event)
-  return (window.Event) 
-    ? e.pageY
-      : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-}
-
-function getCursorX(e) {
-  return (window.Event) 
-    ? e.pageX 
-      : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-}
-
-module.exports = {
-  getCursorXY
-}
+// for testing on index.html
+// window.el = Cursor
