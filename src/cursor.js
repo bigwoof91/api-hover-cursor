81ba2b8acc31a509b632f2fc0d20c5162db68c73
Michael Wolf <wolf@Michaels-MBP.fios-router.home>
2019-03-31 14:02:18 -0400

    0.0.12

--- src/cursor.js
+++ src/cursor.js
@@ -2,17 +2,17 @@ function getCursorXY(log = false) {
   if (window.Event) { // for old browsers; https://developer.mozilla.org/en-US/docs/Web/API/Window/captureEvents
     document.captureEvents(Event.MOUSEMOVE);
   }
-  document.onmousemove = (e) => getCoordinates(e, log);
+  document.onmousemove = event => getCoordinates(event, log);
 };
 
-function logXY({ x, y }) {
+function logXY(x, y) {
   console.log('X:', x, 'Y:', y);
 };
 
 function getCoordinates(e, doLog = false) {
   var y = getCursorY(e);
   var x = getCursorX(e);
-  if (doLog) logXY({ x, y })
+  if (doLog) logXY(x, y);
   return { x, y, }
 };
 
