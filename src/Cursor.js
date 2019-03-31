b2045ad38fad61ac02f8826f25d5189a0d6655ff
Michael Wolf <wolf@Michaels-MBP.fios-router.home>
2019-03-31 00:48:25 -0400

    0.0.9

--- src/Cursor.js
+++ src/Cursor.js
@@ -1,33 +1,33 @@
-
-export default class Cursor {
-  getCursorXY = (log = false) => {
-    if (window.Event) { // for old browsers; https://developer.mozilla.org/en-US/docs/Web/API/Window/captureEvents
+function getCursorXY(log = false) {
+  if (window.Event) { // for old browsers; https://developer.mozilla.org/en-US/docs/Web/API/Window/captureEvents
     document.captureEvents(Event.MOUSEMOVE);
-    }
-    document.onmousemove = (e) => getCoordinates(e, log);
-  };
-  
-  logXY = ({ x, y }) => console.log(`positionX: ${x}, positionY: ${y}`);
-  
-  getCoordinates = (e, doLog = false) => {
-    var y = getCursorY(e);
-    var x = getCursorX(e);
-    if (doLog) logXY({x, y})
-    return {
-      x,
-      y,
-    }
-  };
-  
-  getCursorY = (e) => (window.Event) 
-      ? e.pageY
-        : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+  }
+  document.onmousemove = (e) => getCoordinates(e, log);
+};
 
-  
-  getCursorX = (e) => (window.Event) 
-      ? e.pageX 
-        : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
-  };
+function logXY({ x, y }) {
+  console.log(`positionX: ${x}, positionY: ${y}`);
+}
 
-// for testing on index.html
-// window.el = Cursor
+function getCoordinates(e, doLog = false) {
+  var y = getCursorY(e);
+  var x = getCursorX(e);
+  if (doLog) logXY({ x, y })
+  return { x, y, }
+};
+
+function getCursorY(e) {
+  (window.Event) ?
+  e.pageY :
+  event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+}
+
+function getCursorX(e) {
+  (window.Event) ?
+  e.pageX :
+  event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+}
+
+module.exports = {
+  getCursorXY
+};
