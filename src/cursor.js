f86a64f2fb21ff0e9b4b06caf92fb3f8158b1cab
Michael Wolf <wolf@Michaels-MBP.fios-router.home>
2019-03-31 14:10:45 -0400

    returning x y strings

--- src/cursor.js
+++ src/cursor.js
@@ -17,13 +17,13 @@ function getCoordinates(e, doLog = false) {
 };
 
 function getCursorX(e) {
-  (window.Event) ?
+  return (window.Event) ?
     e.pageX :
     event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
 };
 
 function getCursorY(e) {
-  (window.Event) ?
+  return (window.Event) ?
     e.pageY :
     event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
 };
